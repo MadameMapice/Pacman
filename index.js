fe4ea280325c49bbddd5e9d1648a1661302643f8
Pia <mariapianavach@gmail.com>
2021-10-21 16:50:51 +0200

    Enter

---
+++ index.js
@@ -0,0 +1,96 @@
+
+// 0 es mur, 1 es sol, 2 bombom, 19 colonnes, 22 lignes
+
+let grille =[
+
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,2,0,2,0,0,0,0,0,2,0,2,0,0,2,0],
+    [0,2,2,2,2,0,2,2,2,0,2,2,2,0,2,2,2,2,0],
+    [0,0,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,0,0],
+    [0,1,1,0,2,0,2,2,2,2,2,2,2,0,2,0,1,1,0],
+    [0,0,0,0,2,0,2,0,0,1,0,0,2,0,2,0,0,0,0],
+    [2,2,2,2,2,2,2,0,1,1,1,0,2,2,2,2,2,2,2],
+    [0,0,0,0,2,0,2,0,0,1,0,0,2,0,2,0,0,0,0],
+    [0,1,1,0,2,0,2,2,2,2,2,2,2,0,2,0,1,1,0],
+    [0,0,0,0,2,0,2,0,0,0,0,0,2,0,2,0,0,0,0],
+    [0,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,2,0,2,2,2,2,2,2,2,2,2,2,2,0,2,2,0],
+    [0,0,2,0,2,0,2,0,0,0,0,0,2,0,2,0,2,0,0],
+    [0,2,2,2,2,0,2,2,2,0,2,2,2,0,2,2,2,2,0],
+    [0,2,0,0,0,0,0,0,2,0,2,0,0,0,0,0,0,2,0],
+    [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+]
+
+
+let pacman={
+
+    x:5,
+    y:2,
+    direction:0
+} 
+
+function affichegrille()
+
+
+{ document.getElementById("grille").innerHTML=""
+
+
+for (let i=0; i<22;i++) {
+
+    for (let j=0; j<19;j++) {
+
+        console.log (grille[i][j]);
+
+        let elem=document.createElement("div")
+
+        elem.style.gridRowStart = i+1;
+        elem.style.gridColumnStart = j+1;
+
+        if(grille[i][j]==0){
+
+            elem.className="mur"
+
+        }
+
+        else if (grille[i][j]==1){
+
+            elem.className="sol"
+        }
+      
+        else {
+            elem.className="bonbon"
+        }
+
+        document.getElementById("grille").appendChild(elem)
+
+    }
+}
+
+}setInterval(tourdejeu,500)
+
+function tourdejeu ()  
+{
+    affichegrille();
+
+
+    affichePacman();
+}
+
+function affichePacman()
+{
+    let elem=document.createElement("div")
+
+    elem.className="pacman"
+
+    document.getElementById("grille").appendChild(elem)
+
+    elem.style.gridRowStart = pacman.y;
+    elem.style.gridColumnStart = pacman.x;
+}
+
