a2dcf18b5bf642fb55d9a64cd05db4b56cf05949
Pia <mariapianavach@gmail.com>
2021-11-04 13:32:50 +0100

    commit

--- index.js
+++ index.js
@@ -27,153 +27,144 @@ let grille =[
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ]
 
+document.body.addEventListener("keyup", changeDirection)
 
-document.body.addEventListener("keypress",changeDirection)
-
-let Score=0
+let interval = setInterval(tourdejeu,200)
  
 let pacman={
-
-    x:2,
-    y:2,
-    direction:1
+    x:1,
+    y:2,662
+    direction:0
 } 
-
-let fantome={
+let tabFantome=[{
     x:10,
     y:11,
-    direction:2
+    direction:3
 
-}
+},{
+    x:10,
+    y:11,
+    direction:3
 
-function affichegrille()
+},{
+    x:10,
+    y:11,
+    direction:3
+
+},{
+    x:10,
+    y:11,
+    direction:3
+
+}]
+
+let Score=0
 
 
-{ document.getElementById("grille").innerHTML=""
+function affichegrille(){ 
+    document.getElementById("grille").innerHTML=""
+        for (let i=0; i<22;i++) {
+            for (let j=0; j<19;j++) {
+                let elem=document.createElement("div")
+                elem.style.gridRowStart = i+1;
+                elem.style.gridColumnStart = j+1;
 
+                document.getElementById("grille").appendChild(elem)
 
-for (let i=0; i<22;i++) {
+                if(grille[i][j]==0){
+                    elem.className="mur"
+                }
 
-    for (let j=0; j<19;j++) {
+                else if (grille[i][j]==1){
+                    elem.className="sol"
+                }
+            
+                else {
+                    elem.className="bonbon"
+                }
 
-
-        let elem=document.createElement("div")
-
-        elem.style.gridRowStart = i+1;
-        elem.style.gridColumnStart = j+1;
-
-        if(grille[i][j]==0){
-
-            elem.className="mur"
-
+            }
         }
 
-        else if (grille[i][j]==1){
-
-            elem.className="sol"
-        }
-      
-        else {
-            elem.className="bonbon"
-        }
-
-        document.getElementById("grille").appendChild(elem)
-
-    }
-}
-
-}
-let numinterval = setInterval(tourdejeu,150)
-
-function tourdejeu ()  
-{
-    affichegrille();
-
-    deplacePacman();
-
-    collision();
-
-    sorti();
-
-    manger();
-
-    affichePacman();
-
-    deplaceFantome()
-
-    collisionFantome()
-
-    afficheFantome();
-
-
-    gagne();
-    
 }
 
 function affichePacman()
 {
     let elem=document.createElement("div")
-
-    elem.className="pacman"
-
     document.getElementById("grille").appendChild(elem)
-
+    elem.className="pacman"
     elem.style.gridRowStart = pacman.y;
     elem.style.gridColumnStart = pacman.x;
+
+        if (pacman.direction == 0){
+            elem.style.transform = "rotate(0deg)"
+    }
+    else if (pacman.direction == 1){
+            elem.style.transform = "rotate(270deg)"
+    }
+    else if(pacman.direction == 2){
+            elem.style.transform = "rotate(180deg)"
+    }
+
+    else if(pacman.direction == 3){
+            elem.style.transform = "rotate(90deg)"
+    }
+
+}
+
+function afficheFantome(num){
+        let elem=document.createElement("div")
+
+        elem.className="fantome"+num
+
+        document.getElementById("grille").appendChild(elem)
+
+        elem.style.gridRowStart = tabFantome[num].y;
+        elem.style.gridColumnStart = tabFantome[num].x;
 }
 
 
-function deplacePacman()
-{
+
+
+function deplacePacman(){
     
 
     if (pacman.direction==0){
         pacman.x=pacman.x+1 // droit
     }
     else if (pacman.direction==1){
-        pacman.y=pacman.y-1 // arriba
+        pacman.y=pacman.y-1 // haut
     }
     else if(pacman.direction==2){
         pacman.x=pacman.x-1 // gauche
     }
 
-
     else{
-        pacman.y=pacman.y+1 // abajo
+        pacman.y=pacman.y+1 // bas
     }
 }
 
-function changeDirection(event)
-{
-    // console.log('hola') pour tester la function
-    console.log(event.key)
-
-    if (event.key=="8"){
-        pacman.direction=1
+function deplaceFantome(num){
+    
+    tabFantome[num].direction=getRandomInt(4)
+    if (tabFantome[num].direction==0){
+        tabFantome[num].x=tabFantome[num].x+1 // droit
     }
-    else if (event.key=="2"){
-        pacman.direction=3
-        
+    else if (tabFantome[num].direction==1){
+        tabFantome[num].y=tabFantome[num].y-1 // haut
     }
-    else if(event.key=="4"){
-        pacman.direction=2
+    else if(tabFantome[num].direction==2){
+        tabFantome[num].x=tabFantome[num].x-1 // gauche
     }
-    else if(event.key=="6"){
-        pacman.direction=0
-    }
-
-    else{
-        console.log("touche non géré")
+    else if(tabFantome[num].direction==3){
+        tabFantome[num].y=tabFantome[num].y+1 // bas
     }
 }
 
-function collision()
-{
+
+function collision(){
     if(grille[pacman.y-1][pacman.x-1]==0){
 
-        console.log("mur")
-
-    
         if (pacman.direction==0){
             pacman.x=pacman.x-1 
         }
@@ -184,121 +175,158 @@ function collision()
             pacman.x=pacman.x+1 
         }
     
-    
-        else{
+        else if(pacman.direction==3){
             pacman.y=pacman.y-1 
         }
+
+    }
+}
+
+function collisionFantome(num){
+    if(grille[tabFantome[num].y-1][tabFantome[num].x-1]==0){
+
+        if (tabFantome[num].direction==0){
+            tabFantome[num].x=tabFantome[num].x-1 
+        }
+        else if (tabFantome[num].direction==1){
+            tabFantome[num].y=tabFantome[num].y+1 
+        }
+        else if(tabFantome[num].direction==2){
+            tabFantome[num].x=tabFantome[num].x+1 
+        }
     
-
+        else if(tabFantome[num].direction==3){
+            tabFantome[num].y=tabFantome[num].y-1 
+        }
+    
     }
-
 }
 
-function manger()
-{
+function manger(){
     if(grille[pacman.y-1][pacman.x-1]==2){
-
         grille[pacman.y-1][pacman.x-1]=1
-
-    Score=Score+10
-    document.querySelector(".score").innerHTML="<h1> score : "+Score+"</h1>"
-     
+        Score=Score+10
+        document.querySelector(".score").innerHTML="<h1> score : "+Score+"</h1>"
     }
 }
 
-function sorti()
-{
+function sorti(){
     if (pacman.x<1){
         pacman.x=19
     }
-
     if (pacman.x>19){
         pacman.x=1
     }
- 
-}
-
-function gagne()
-{
-   
-let gagne = true
-
-for (let i=0; i<22;i++) {
-
-for (let j=0; j<19;j++) {
-    if(grille[i][j]==2){
-
-       gagne=false
-
-
+    if (pacman.y < 1){
+        pacman.y = 22
+    }
+    if (pacman.y > 22){
+        pacman.y = 1
     }
 }
-} if (
-    gagne==true
-) {
-    alert("Vous avez gagné!")
 
-    clearInterval(numinterval)
+function sortiFantome(num){
+    if (tabFantome[num].x<1){
+        tabFantome[num].x=19
+    }
+    if (tabFantome[num].x>19){
+        tabFantome[num].x=1
+    }
+    if (tabFantome[num].y < 1){
+        tabFantome[num].y = 22
+    }
+    if (tabFantome[num].y > 22){
+        tabFantome[num].y = 1
+    }
 }
+
+function gagne(){
+    let gagne = true
+        for (let i=0; i<22;i++) {
+            for (let j=0; j<19;j++) {
+                if(grille[i][j]==2){
+                    gagne=false
+
+            }
+        }
+    }
+}
+
+if (gagne==true) {
+    alert("Vous avez gagné!")
+    clearInterval(interval)
+
         
 }
 
-function afficheFantome()
-{
-    let elem=document.createElement("div")
 
-    elem.className="fantome"
+function tourdejeu () {
+    affichegrille();
 
-    document.getElementById("grille").appendChild(elem)
+    deplaceFantome(0);
 
-    elem.style.gridRowStart = fantome.y;
-    elem.style.gridColumnStart = fantome.x;
+    deplaceFantome(1);
+
+    deplaceFantome(2);
+
+    deplaceFantome(3);
+
+    sortiFantome(0);
+
+    sortiFantome(1);
+
+    sortiFantome(2);
+
+    sortiFantome(3);
+
+    collisionFantome(0);
+
+    collisionFantome(1);
+
+    collisionFantome(2);
+
+    collisionFantome(3);
+
+    afficheFantome(0);
+
+    afficheFantome(1);
+
+    afficheFantome(2);
+
+    afficheFantome(3);
+
+    deplacePacman();
+
+    sorti();
+
+    collision();
+
+    manger();
+
+    affichePacman();
+
+    gagne();
+    
 }
 
-function deplaceFantome()
-{
-    
-fantome.direction=getRandomInt(4)
-    if (fantome.direction==0){
-        fantome.x=fantome.x+1 // droit
+function changeDirection(event){
+    console.log(event.key)
+
+    if (event.key=="8"){
+        pacman.direction=1
     }
-    else if (fantome.direction==1){
-        fantome.y=fantome.y-1 // arriba
+    else if (event.key=="2"){
+        pacman.direction=3
     }
-    else if(fantome.direction==2){
-        fantome.x=fantome.x-1 // gauche
+    else if(event.key=="4"){
+        pacman.direction=2
     }
-
-
-    else{
-        fantome.y=fantome.y+1 // abajo
-    }
-}
-
-function collisionFantome()
-{
-    if(grille[fantome.y-1][fantome.x-1]==0){
-
-    
-        if (fantome.direction==0){
-            fantome.x=fantome.x-1 
-        }
-        else if (fantome.direction==1){
-            fantome.y=fantome.y+1 
-        }
-        else if(fantome.direction==2){
-            fantome.x=fantome.x+1 
-        }
-    
-    
-        else{
-            fantome.y=fantome.y-1 
-        }
-    
-
+    else if(event.key=="6"){
+        pacman.direction=0
     }
 
 }
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
